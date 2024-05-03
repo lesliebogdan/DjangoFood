fbca549ff533ecb4afd9eca666a6ef152da0ea21
leslie bogdan <leslie@lesliebogdan.com>
2024-05-03 16:53:34 +1000

    register user functionalities and other setups

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,7 +8,7 @@ def post_save_create_profile_reciever(sender, instance, created,**kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print('user profile is created')
+        #print('user profile is created')
 
     else:
         try:
@@ -17,13 +17,14 @@ def post_save_create_profile_reciever(sender, instance, created,**kwargs):
         except:
             # new profile needs to be created if it does not exist
             UserProfile.objects.create(user=instance)
-            print('profile did not exist, user profile is created')
+            #print('profile did not exist, user profile is created')
 
-        print('user is updated')
+        #print('user is updated')
 
 
 # will trigger before the user is created
 @receiver(pre_save, sender = User)
 def pre_save_profile_receiver(sender, instance, **kwags):
-    print(instance.username, 'this user is being saved')
+    pass
+    #print(instance.username, 'this user is being saved')
     
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm
+from .models import User, UserProfile
+from django.contrib import messages
 
 # Create your views here.
+def registerUser(request):
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+
+
+            # use the 'create_user' method we already have in our User model
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+
+            user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                password = password,
+            )
+            user.role = User.CUSTOMER
+            user.save()
+
+            # message the user
+            messages.success(request, 'Your account has been registered successfully!')
+
+            return redirect('registerUser')
+        else:
+            print('invalid form submission')
+            print(form.errors)
+    else:
+        form = UserForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'accounts/registerUser.html',context)

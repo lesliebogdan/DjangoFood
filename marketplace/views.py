38524534d24f89951ff53dce1c4243a25b3b1540
leslie bogdan <leslie@lesliebogdan.com>
2024-05-23 11:25:41 +1000

    more location work

--- marketplace/views.py
+++ marketplace/views.py
@@ -11,6 +11,10 @@ from django.http import HttpResponse, JsonResponse
 
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D
+from django.contrib.gis.db.models.functions import Distance
+
 # Create your views here.
 
 def marketplace(request):
@@ -145,15 +149,15 @@ def search(request):
         fetch_vendors_by_fooditems = FoodItem.objects.filter(food_title__icontains=keyword, is_available=True).values_list('vendor', flat=True)
         
         vendors = Vendor.objects.filter(Q(id__in=fetch_vendors_by_fooditems) | Q(vendor_name__icontains=keyword, is_approved=True, user__is_active=True))
-        #if latitude and longitude and radius:
-        #    pnt = GEOSGeometry('POINT(%s %s)' % (longitude, latitude))
+        if latitude and longitude and radius:
+            pnt = GEOSGeometry('POINT(%s %s)' % (longitude, latitude))
 
-        #    vendors = Vendor.objects.filter(Q(id__in=fetch_vendors_by_fooditems) | Q(vendor_name__icontains=keyword, is_approved=True, user__is_active=True),
-        #    user_profile__location__distance_lte=(pnt, D(km=radius))
-        #    ).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+            vendors = Vendor.objects.filter(Q(id__in=fetch_vendors_by_fooditems) | Q(vendor_name__icontains=keyword, is_approved=True, user__is_active=True),
+            user_profile__location__distance_lte=(pnt, D(km=radius))
+            ).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
 
-        #    for v in vendors:
-        #        v.kms = round(v.distance.km, 1)
+            for v in vendors:
+                v.kms = round(v.distance.km, 1)
         vendor_count = vendors.count()
         context = {
             'vendors': vendors,
